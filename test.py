acd58123bcfe9fdaa7ecac1895b5cfc7bb16bcff
siperman <690400412@qq.com>
2021-08-27 18:23:29 +0800

    m

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import jenkins
 import time
+import json
 
 server = jenkins.Jenkins('http://localhost:8080', username='test', password='test')
 user = server.get_whoami()
@@ -10,18 +11,23 @@ print('Hello %s from Jenkins %s' % (user['fullName'], version))
 # print(server.get_all_jobs())
 for num in range(0,len(server.get_all_jobs())):
       job_name = server.get_all_jobs()[num]['fullname']
-      print(server.get_job_info(job_name))
+      print(json.dumps(server.get_job_info(job_name)))
       try:
         lastCompletedBuild_num = server.get_job_info(job_name)['lastCompletedBuild']['number']
       except:
         continue
     #获取最后一次完成构建的时间戳，单位由毫秒转换为秒
+      info = server.get_build_info(job_name,lastCompletedBuild_num)
+      print(json.dumps(info))
       lastCompletedBuild_timestamp = server.get_build_info(job_name,lastCompletedBuild_num)['timestamp'] / 1000
       #将时间先由秒转化为元组在转化为字符串并取到天数
       lastCompletedBuild_date = time.strftime("%Y%m%d",time.localtime(lastCompletedBuild_timestamp))
+      now=time.localtime( time.time() )
       print(lastCompletedBuild_date)
-      server.disable_job(job_name)
-      server.delete_job(job_name)
+      print(now)
+      print(time.time()-lastCompletedBuild_timestamp)
+    #   server.disable_job(job_name)
+    #   server.delete_job(job_name)
 
 # from jenkinsapi.jenkins import Jenkins
 
